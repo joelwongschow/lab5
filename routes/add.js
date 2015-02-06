6a08c6725dd593ea0cab2d1ddabc24a94555dd31
Joel Schow <jwschow@ucsd.edu>
2015-02-06 15:09:27 -0800

    lab complete

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,13 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+exports.addFriend = function(req, res) {   
+	console.log(data);
+	res.render('add', { "friends" : data.friends });
+	
+	var newFriend = { 'name' : req.query.name };
+	newFriend.name = req.query.name;
+	newFriend.description = req.query.description;
+	newFriend.imageURL = "http://lorempixel.com/400/400/people";
+	
+	data["friends"].push(newFriend);
+}
